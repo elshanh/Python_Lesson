ce468962eac3605c738d06586d783ea86ec3bd73
Elshan HUSEYNZADA <elshan.huseynzada@outlook.com>
2021-01-10 02:07:23 +0400

    Lesson Python Unit Type Integer & Float

---
+++ 03.Python.Lesson.TypeIntegerFloat.py
@@ -0,0 +1,15 @@
+a1 = 15 + 15;
+print(a1)
+print(type(a1))
+print('*********************')
+a2 = 15.5 + 15.5
+print(a2)
+print(type(a2))
+print('*********************')
+a3 = 15 + 15.5
+print(a3)
+print(type(a3))
+print('*********************')
+a4 = 15 + '15'
+print(a4)
+print(type(a4))
